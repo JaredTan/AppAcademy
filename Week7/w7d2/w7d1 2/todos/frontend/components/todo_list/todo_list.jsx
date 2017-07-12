ac66b6a6db80b0712b4d519c5ee5727193035e2c
JaredTan <jared.e.tan@gmail.com>
2017-07-11 18:08:40 -0700

    Full Todo App + Middleware. Rails + React

---
+++ Week7/w7d2/w7d1 2/todos/frontend/components/todo_list/todo_list.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react';
+import allTodos from '../../reducers/selectors';
+import TodoListItem from './todo_list_item';
+import TodoForm from './todo_form';
+
+
+const TodoList = ({todos, receiveTodo, removeTodo}) => {
+  return (
+    <div>
+        <ul>
+          {todos.map( (todo,idx) => <TodoListItem todo={todo} key={idx} removeTodo={removeTodo} receiveTodo={receiveTodo}/>)}
+        </ul>
+        <TodoForm receiveTodo={receiveTodo}/>
+      </div>
+  );
+};
+
+
+
+export default TodoList;
