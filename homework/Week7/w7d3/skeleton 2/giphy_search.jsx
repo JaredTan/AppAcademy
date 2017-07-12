7b86034d445aa3f9b77673ae30a7c105f97c9bb3
JaredTan <jared.e.tan@gmail.com>
2017-07-12 08:23:32 -0700

    Giphys!

---
+++ homework/Week7/w7d3/skeleton 2/giphy_search.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+import Root from './components/root';
+import * as APIUtil from './util/api_util.js';
+import {fetchSearchGiphys} from './actions/giphy_actions'
+
+document.addEventListener('DOMContentLoaded', () => {
+//
+	const store = configureStore();
+  console.log(store);
+	// const root = document.getElementById('root');
+	// ReactDOM.render(<Root store={store} />, root);
+//
+  window.store = store;
+  // window.receiveSearchGiphys = receiveSearchGiphys();
+  window.fetchSearchGiphys = APIUtil.fetchSearchGiphys;
+});
---
+++ homework/Week7/w7d3/solution 3/giphy_search.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+import Root from './components/root';
+import * as APIUtil from './util/api_util';
+
+document.addEventListener('DOMContentLoaded', () => {
+	const store = configureStore();
+	const root = document.getElementById('root');
+	ReactDOM.render(<Root store={store} />, root);
+	window.store = store;
+  // window.receiveSearchGiphys = receiveSearchGiphys();
+  window.fetchSearchGiphys = APIUtil.fetchSearchGiphys;
+});
